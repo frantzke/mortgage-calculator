c2386e1ddfd5fe0c3a9ccbc7aef36c104aa3cd52
frantzke <lukas.frantzke@mail.utoronto.ca>
2022-08-10 19:24:02 -0400

    Add deployment config

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,13 @@
-const { defineConfig } = require('@vue/cli-service');
+const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
 
+  publicPath: process.env.NODE_ENV === "production" ? "/mortgage-calculator/" : "/",
+
   pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-  }
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    },
+  },
 });
